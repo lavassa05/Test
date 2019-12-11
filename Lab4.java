eb123b77c3b4708c5dcda030138e41d1b58c0702
Your Name <you@example.com>
2019-12-11 12:42:31 +0530

    first commit using gitbash

---
+++ Lab4.java
@@ -0,0 +1,13 @@
+class Lab4{
+public static void main(String args[]){
+
+String b= "";
+String s= "abc";
+String i= "123";
+
+System.out.println(b);
+System.out.println("s"+i);
+System.out.println(i+s);
+
+}
+}
---
+++ vaibhav.java
@@ -0,0 +1,8 @@
+class vaibhav
+{
+public static void main (String args[])
+{
+System.out.println("java by kiran");
+System.out.println("karvenagar");
+}
+}
